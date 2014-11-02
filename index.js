e05d5c986ce194cd704882fdc5a3f03f786eca59
b3rn475 <b3rn4@hotmail.it>
2014-11-02 22:05:56 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*jslint node: true, nomen: true, es5: true */
+/** 
+ * Developed By Carlo Bernaschina (GitHub - B3rn475) 
+ * www.bernaschina.com 
+ * 
+ * Distributed under the MIT Licence 
+ */
+"use strict";
+
+module.exports.Server = require('./lib/server');
+module.exports.Client = require('./lib/client');
---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "proxied-socket",
+  "version": "0.0.1",
+  "description": "",
+  "keywords": ["socket","proxy"],
+  "homepage": "https://github.com/B3rn475/proxied-socket",
+  "author": "B3rn475",
+  "licenses": {
+    "type": "MIT",
+    "url": "http://github.com/B3rn475/proxied-socket/raw/master/LICENSE"
+  },
+  "repository": {
+    "type": "git",
+    "url": "http://github.com/B3rn475/proxied-socket.git"
+  },
+  "dependencies": {
+  },
+  "engines": {"node":">=0.8.0"},
+  "main": "./index.js"
+}
