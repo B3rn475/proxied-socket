f5d14d04cb8178680d8a95969a57a0cc8ff234d4
Ben Timby <btimby@smartfile.com>
2018-03-06 12:17:02 -0500

    Tests and fixes.

---
+++ test/header/haproxy.js
@@ -0,0 +1,50 @@
+/*jslint node: true, nomen: true, es5: true */
+/**
+ * Developed By Carlo Bernaschina (GitHub - B3rn475)
+ * www.bernaschina.com
+ *
+ * Distributed under the MIT Licence
+ */
+/*global describe, it, beforeEach*/
+"use strict";
+
+var streams = require('memory-streams'),
+    assert = require('assert'),
+    createParser = require('../../lib/server/header/haproxy'),
+    sinon = require('sinon');
+
+describe('Client', function () {
+    it('should be a function', function () {
+        assert.equal(typeof createParser, 'function');
+    });
+    it('should return a function', function () {
+        var onConnection = sinon.spy(),
+            onHeaderError = sinon.spy(),
+            parser = createParser({}, onConnection, onHeaderError);
+
+        assert.equal(typeof parser, 'function');
+    });
+    it('should reject invalid headers preamble', function () {
+        var socket = new streams.ReadableStream(new Buffer('FOOBAR TCP4 1.2.3.4 5.6.7.8 100 200\n')),
+            onConnection = sinon.spy(),
+            onHeaderError = sinon.spy(),
+            parser = createParser(socket, onConnection, onHeaderError);
+
+        parser.call(socket);
+
+        assert.ok(!onConnection.called);
+        assert.ok(onHeaderError.calledOnce);
+    });
+    it('should parse address', function () {
+        var socket = new streams.ReadableStream(new Buffer('PROXY TCP4 1.2.3.4 5.6.7.8 100 200\n')),
+            onConnection = sinon.spy(),
+            onHeaderError = sinon.spy(),
+            parser = createParser(socket, onConnection, onHeaderError);
+
+        parser.call(socket);
+
+        assert.ok(onConnection.calledOnce);
+        assert.ok(onConnection.calledWith('1.2.3.4'));
+        assert.ok(!onHeaderError.called);
+    });
+});
